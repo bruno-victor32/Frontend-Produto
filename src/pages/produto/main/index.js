67a4bdba46c2637da6e0d93f373cc8af2b15d0e9
Bruno Victor da Silva Vicente <bruno.victor32@yahoo.com.br>
2021-06-03 19:44:39 -0300

    Trabalhando com o Bootstrap

--- src/pages/produto/main/index.js
+++ src/pages/produto/main/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
  
 export default class Main extends Component {
     constructor(props) {
@@ -22,19 +23,43 @@ export default class Main extends Component {
  
     render() {
         const { produto } = this.state;
-        return produto.map((produto, index) => (
  
+        return (
             <div className="produto-list">
-                <div key={index} className="card mb-4">
-                    <h5 className="card-header">{produto.nomeProduto}</h5>
+                <Link to={`/criarproduto`}> <button type="button" class="btn btn-success">Novo</button> </Link>
+                <br /><br />
  
-                    <article key={produto._id}>
-                        <strong> {produto.preco} </strong>
-                        <p> <Link to={`/produtos/${produto.id}`}> Acessar </Link> </p>
-                        <br />
-                    </article>
-                </div>
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Nome</th>
+                            <th scope="col">Descrição do Produto</th>
+                            <th scope="col">Preço</th>
+                            <th scope="col">Qtd de Estoque</th>
+                            <th scope="col">Data de Compra</th>
+                            <th scope="col">Ativo</th>
+                            <th scope="col">Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {produto.map((produto, index) => (
+                            <tr>
+                                <th scope="row">{produto.id}</th>
+                                <td>{produto.nomeProduto}</td>
+                                <td>{produto.descricaoProduto}</td>
+                                <td>{produto.preco.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</td>
+                                <td>{produto.quantidadeEstoque}</td>
+                                <td>{new Date(produto.datadeCompra).toLocaleDateString('pt-BR', { year: 'numeric', month: '2-digit', day: '2-digit' })}</td>
+                                <td>{produto.ativo ? "Sim" : "Não"}</td>
+                                <td> <Link to={`/produtos/${produto.id}`}> <button type="button" class="btn btn-primary">Detalhes</button> </Link> </td>
+                                <td><button type="button" class="btn btn-warning">Atualizar</button></td>
+                                <td><button type="button" class="btn btn-danger">Excluir</button></td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
             </div>
-        ))
-    };
+        )
+    }
 }
